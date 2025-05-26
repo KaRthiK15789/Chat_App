d1249ce64d9d80e9a9dd3745f35587ef1de7fef5
KARTHIK <144221056+KaRthiK15789@users.noreply.github.com>
2025-05-26 18:07:02 +0530

    Update next.config.js

--- chat-app-assignment/next.config.js
+++ chat-app-assignment/next.config.js
@@ -1,36 +1,6 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
+
 const nextConfig = {
-  images: {
-    unoptimized: true,
-    domains: [
-      "source.unsplash.com",
-      "images.unsplash.com",
-      "ext.same-assets.com",
-      "ugc.same-assets.com",
-    ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ext.same-assets.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ugc.same-assets.com",
-        pathname: "/**",
-      },
-    ],
-  },
   async headers() {
     return [
       {
@@ -42,8 +12,9 @@ const nextConfig = {
               default-src 'self';
               script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net;
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net;
+              style-src-elem 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net;
               font-src 'self' https://fonts.gstatic.com;
-              img-src 'self' data: https://*;
+              img-src 'self' data: https:;
               connect-src *;
               object-src 'none';
               frame-src 'none';
