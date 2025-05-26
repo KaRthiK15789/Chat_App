831ca46ccdfb4dc5d39b88be2df0e89aa9e7f633
KARTHIK <144221056+KaRthiK15789@users.noreply.github.com>
2025-05-26 17:50:24 +0530

    Update next.config.js

--- chat-app-assignment/next.config.js
+++ chat-app-assignment/next.config.js
@@ -7,15 +7,17 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; " +
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net; " +
-              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; " +
-              "style-src-elem 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net; " +
-              "font-src 'self' https://fonts.gstatic.com https://cdn.jsdelivr.net; " +
-              "img-src 'self' data: https:; " +
-              "connect-src *; " +
-              "frame-src 'none';",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' cdn.jsdelivr.net;
+              style-src 'self' 'unsafe-inline' fonts.googleapis.com cdn.jsdelivr.net;
+              style-src-elem 'self' fonts.googleapis.com cdn.jsdelivr.net;
+              font-src 'self' fonts.gstatic.com cdn.jsdelivr.net;
+              img-src 'self' data: https:;
+              connect-src *;
+              object-src 'none';
+              frame-src 'none';
+            `.replace(/\s{2,}/g, " ").trim(),
           },
         ],
       },
